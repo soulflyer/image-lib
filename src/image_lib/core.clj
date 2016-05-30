be327a87996e22972ffe3a2b3e82238be6154d6d
iain <iain@soulflyer.co.uk>
2016-05-30 23:10:05 +0700

    added preferences write

--- src/image_lib/core.clj
+++ src/image_lib/core.clj
@@ -117,3 +117,12 @@
   "return the value of the preference from the db"
   [db preferences-collection pref]
   (:path (first (mc/find-maps db preferences-collection {:_id pref}))))
+
+(defn preferences
+  "return all the preferences"
+  [db preferences-collection]
+  (mc/find-maps db preferences-collection))
+(defn preference!
+  "set the value of preference in the db"
+  [db preferences-collection pref value]
+  (mc/update db preferences-collection {:_id pref} {$set {:path value}} {:upsert true}))
