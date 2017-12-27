d6251af4d6b598fdd37592b1f7ecece80e8dfbcb
iain <iain@soulflyer.co.uk>
2017-12-27 23:11:56 +0700

    refactor untested

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject image-lib "0.2.1-SNAPSHOT"
+(defproject image-lib "0.2.2-SNAPSHOT"
   :description "A collection of functions to retrieve data about images and keywords from a mongo database"
   :url "http://github.com/soulflyer/image-lib"
   :license {:name "Eclipse Public License"
